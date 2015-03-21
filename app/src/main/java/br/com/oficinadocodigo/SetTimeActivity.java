f22cfb0d9a307b76dca8bc0ba680c15484ac2579
Henry <carloshenry.wm@gmail.com>
2015-03-21 03:18:09 -0300

    Fiz a transicao da tela que mostra os cara que vao jogar

--- app/src/main/java/br/com/oficinadocodigo/SetTimeActivity.java
+++ app/src/main/java/br/com/oficinadocodigo/SetTimeActivity.java
@@ -1,5 +1,6 @@
 package br.com.oficinadocodigo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.Arrays;
@@ -15,10 +17,10 @@ import br.com.oficinadocodigo.R;
 import br.com.oficinadocodigo.aux.Message;
 import br.com.oficinadocodigo.aux.TempGameData;
 
-public class SetTimeActivity extends ActionBarActivity {
+public class SetTimeActivity extends ActionBarActivity implements View.OnClickListener {
 
     private Spinner spinnerSetTime;
-
+    private Button btn_save_adjust;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,9 @@ public class SetTimeActivity extends ActionBarActivity {
 
 
         spinnerSetTime = (Spinner) findViewById(R.id.spinner_timer_setTime);
+        btn_save_adjust = (Button) findViewById(R.id.btn_save_adjust);
+
+        btn_save_adjust.setOnClickListener(this);
 
         fillSpinner();
 
@@ -61,4 +66,18 @@ public class SetTimeActivity extends ActionBarActivity {
         spinnerSetTime.setAdapter(users);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_save_adjust:
+                trocarTela(ShowOpponentsActivity.class);
+            break;
+        }
+    }
+
+
+
+    private void trocarTela(Class c){
+        startActivity(new Intent(SetTimeActivity.this,c));
+    }
 }
